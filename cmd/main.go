87179c3a992ade9983454b44dad4e8efc222a873
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2022-04-19 12:35:28 +0530

    ex 3: Convert into an api endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
+	"time"
+
+	"github.com/go-chi/chi"
 )
 
+type Handler struct {
+	ctx       context.Context
+	ctxCancel context.CancelFunc
+}
+
 func main() {
-	//
 	ctx, ctxCancel := context.WithCancel(context.Background())
+	hand := &Handler{
+		ctx:       ctx,
+		ctxCancel: ctxCancel,
+	}
 
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
@@ -20,16 +34,35 @@ func main() {
 		<-ch
 		// fmt.Println("Interrupt received")
 		ctxCancel()
-		// fmt.Println("Good Bye!")
-		// os.Exit(0)
+		time.Sleep(time.Second * 1)
+		fmt.Println("Good Bye!")
+		os.Exit(0)
 	}()
 
-	num := 10
+	router := chi.NewRouter()
+	// http://localhost:8090/cpu?num=10
+	router.Get("/cpu", hand.cpuHandler)
+
+	log.Println("Starting server on port:", 8090)
+	if err := http.ListenAndServe(":8090", router); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (h *Handler) cpuHandler(w http.ResponseWriter, r *http.Request) {
+	nums := r.URL.Query().Get("num")
+	fmt.Println(nums)
+	num, err := strconv.Atoi(nums)
+	if err != nil {
+		fmt.Fprintf(w, "invalid input %s\n", nums)
+		return
+	}
+
 	var wg sync.WaitGroup
 	wg.Add(num)
 
 	for i := 1; i <= num; i++ {
-		go cpuBound(i, ctx, &wg)
+		go cpuBound(i, h.ctx, &wg)
 	}
 
 	wg.Wait()
